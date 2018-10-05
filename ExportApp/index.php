fcedc12372634a33bfac34375053d17b4114dd69
Imrassoft <mutumba.sowed@gmail.com>
2018-10-05 22:24:41 +0300

    input layout changed

--- ExportApp/index.php
+++ ExportApp/index.php
@@ -10,18 +10,19 @@
 <body>
 <nav class="navbar navbar-light bg-light">
 <div class="container">
-  <a class="navbar-brand" href="#">Data Export Tool</a>
+  <a class="navbar-brand" href="#">LVCT Data Export Tool</a>
   </div>
 </nav>
 <br><br>
 
-<div class="container" style="padding-top: 10%">
+<div class="container" style="padding-top: 5%">
  <form action="export.php" class="form-horizontal"  role="form" method="post">
         <fieldset>
             <legend>Select time period to export data</legend>
 			<span>NB: This Tool exports records from the voice system only</span><br><br>
-				<div class="row" align="center">
+				<div class="row">
 				<div class="col-lg-4">
+				<label>Start Date</label>
 					  <div class="input-group date form_datetime" data-date="2018-09-16T05:25:07Z" data-date-format="yyyy-mm-dd HH:mm:ss" data-link-field="dtp_input1">
 						<input class="form-control" size="16" type="text" value="" name="fromDate" readonly>
 						<span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
@@ -29,6 +30,7 @@
                 </div>
 				</div>
 				<div class="col-lg-4">
+				<label>End Date</label>
 					  <div class="input-group date form_datetime" data-date="2018-09-16T05:25:07Z" data-date-format="yyyy-mm-dd HH:mm:ss" data-link-field="dtp_input1">
 						<input class="form-control" size="16" type="text" value="" name="toDate" readonly>
 						<span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
@@ -36,6 +38,7 @@
                 </div>
 				</div>
 				<div class="col-lg-4">
+				<br>
 					<input class="btn btn-info btn-large" type="submit" id="" value="Export to CSV" /><br/>
 				</div>
 			</div>
